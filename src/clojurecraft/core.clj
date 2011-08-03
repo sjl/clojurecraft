1eabe11633bb2ac0bcb4dd538a7dc6f3de92707f
Steve Losh <steve@stevelosh.com>
2011-08-03 19:30:35 -0400

    Remove scratch stuff.

--- src/clojurecraft/core.clj
+++ src/clojurecraft/core.clj
@@ -117,15 +117,3 @@
   (dosync (alter (:connection bot) merge {:exit true})))
 
 
-; Scratch --------------------------------------------------------------------------
-(def minecraft-local {:name "localhost" :port 25565})
-
-;(def bot (connect minecraft-local nil))
-;(act/move bot -2 0 0)
-;(pprint @(:packet-counts-in bot))
-;(pprint @(:packet-counts-out bot))
-;(pprint (:player bot))
-;(pprint (:world bot))
-;(println (:loc @(:player bot)))
-;(disconnect bot)
-
